99565fe751612e56c4c737dbfca25b69fac95661
Abarbesgaard <Abarbesgaard@gmail.com>
2023-09-04 12:42:04 +0200

    sale virker

---
+++ ReolMarkedet/ReolMarkedet/Model/Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReolMarkedet.Model.Enum;
+
+namespace ReolMarkedet.Model
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string Tenant { get; set; } 
+        public string Barcode { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; } 
+        public string Place { get; set; }
+
+        public TypeEnum Type{ get; set; }
+        public float Discount { get; set; }
+
+        public Item(
+            int id,  
+            string tenant,
+            string barcode, 
+            string name,
+            float price,
+            string place,
+            TypeEnum type, 
+            float discount)
+        {
+            this.ID = id;
+            this.Tenant = tenant;
+            this.Barcode = barcode;
+            this.Name = name;
+            this.Price = price;
+            this.Place = place;
+            this.Type = type;
+            this.Discount = discount;
+        }
+    }
+}
--- ReolMarkedet/ReolMarkedet/Program.cs
+++ ReolMarkedet/ReolMarkedet/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ReolMarkedet
+﻿using ReolMarkedet.Model;
+
+namespace ReolMarkedet
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ændret tekst!");
+            Item item0 = new Item(1,"sdf","123123123","Ole Hansen", 1.54f,"reol", Model.Enum.TypeEnum.Clothes, 0.25f);
+            Item item1 = new Item(1,"sdf","123123123","Ole Hansen", 1.54f,"reol", Model.Enum.TypeEnum.Clothes, 0.25f);
+            Item item2 = new Item(1, "sdf", "123123123", "Ole Hansen", 1.54f, "reol", Model.Enum.TypeEnum.Clothes, 0.25f);
+
+
+            ItemSale itemSale = new ItemSale();
+            itemSale.items.Add(item0);
+            itemSale.items.Add(item1);
+            itemSale.items.Add(item2);
+
+            itemSale.CreateSale();
+            Console.ReadLine();
         }
     }
 }
